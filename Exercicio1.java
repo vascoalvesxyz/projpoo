731b1681acef651228f41f298cd29968317a03d1
Vasco <true.vasco.alves@gmail.com>
2024-09-25 12:07:32 +0100

    0

---
+++ Exercicio1.java
@@ -0,0 +1,29 @@
+// Considere um centro de saúde onde trabalham médicos de diferentes especialidades. A componente fixa do ordenado dos médicos é calculada a partir do salário base da especialidade acrescido de 4% por cada 5 de anos de serviço. A componente variável do ordenado depende do número de horas de trabalho extra. Pretende desenvolver-se uma aplicação para gerir o centro de saúde considerando um número indeterminado de médicos e de especialidades. A aplicação deverá calcular os ordenados a pagar por médico e por especialidade.
+//
+// String[] especialidades = { 
+     //nome/salário base/custo da hora extra 
+     //"Radiologia/2030/50",  
+     //"Oftalmologia/2500/70", 
+     //"Pediatria/2700/75" 
+// }; 
+//
+//String[] médicos = { 
+//nome/especialidade/anos de serviço/horas extra 
+//"Vasco Santana/radiologia/15/10", 
+//"Laura Alves/oftalmologia/5/7", 
+//"António Silva/oftalmologia/12/5" 
+//}; 
+//
+//Resultado: 
+//Vasco Santana: 2783.5€ 
+//Laura Alves: 3090.0€ 
+//António Silva: 3054.0€ 
+//
+//Radiologia: 2783.5€ 
+//Oftalmologia: 6144.0€
+
+public class Exercicio1 {
+    private static print_especialidadedes() {}; 
+    private static print_medicos() {}; 
+
+}
